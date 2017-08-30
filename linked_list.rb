2975465e251194566e9037432d7ce59cd3ceef56
Tehut <tehut.m.g@gmail.com>
2017-08-30 16:03:12 -0700

    implemented instert and search

--- linked_list.rb
+++ linked_list.rb
@@ -19,13 +19,35 @@ class LinkedList
   # method to add a new node with the specific data value in the linked list
   # insert the new node at the beginning of the linked list
   def insert(value)
-    puts "Not implemented"
+    new_node = Node.new(value)
+   if @head 
+    new_node.next = @head
+    @head = new_node
+  else
+    @head = new_node
+  end
+   puts @head.data
+
   end
 
   # method to find if the linked list contains a node with specified value
   # returns true if found, false otherwise
   def search(value)
-    puts "Not implemented"
+    found = false
+      current_node = @head
+    until found  do 
+      if current_node.data == value 
+         found = true
+         break
+      else
+        if current_node.next 
+          current_node = current_node.next
+        else
+          break 
+        end
+      end
+    end
+      puts found
   end
 
   # method to return the max value in the linked list
@@ -118,112 +140,116 @@ my_linked_list.insert(5)
 my_linked_list.insert(3)
 my_linked_list.insert(1)
 
-# print all elements
-puts "Printing elements in the linked list:"
-my_linked_list.visit
+my_linked_list.search(5)
+my_linked_list.search(4)
+my_linked_list.search(3)
 
-# Find the value at the nth node
-puts "Confirming values in the linked list using find_nth_from_beginning method."
-value = my_linked_list.find_nth_from_beginning(2)
-puts "BUG: Value at index 2 should be 5 and is #{value}" if value != 5
-value = my_linked_list.find_nth_from_beginning(1)
-puts "BUG: Value at index 1 should be 3 and is #{value}" if value != 3
-value = my_linked_list.find_nth_from_beginning(0)
-puts "BUG: Value at index 0 should be 1 and is #{value}" if value != 1
+# # print all elements
+# puts "Printing elements in the linked list:"
+# my_linked_list.visit
 
-# print all elements
-puts "Printing elements in the linked list:"
-my_linked_list.visit
+# # Find the value at the nth node
+# puts "Confirming values in the linked list using find_nth_from_beginning method."
+# value = my_linked_list.find_nth_from_beginning(2)
+# puts "BUG: Value at index 2 should be 5 and is #{value}" if value != 5
+# value = my_linked_list.find_nth_from_beginning(1)
+# puts "BUG: Value at index 1 should be 3 and is #{value}" if value != 3
+# value = my_linked_list.find_nth_from_beginning(0)
+# puts "BUG: Value at index 0 should be 1 and is #{value}" if value != 1
 
-# Insert ascending
-puts "Adding 4 in ascending order."
-my_linked_list.insert_ascending(4)
-# check newly inserted value
-puts "Checking values by calling find_nth_from_beginning method."
-value = my_linked_list.find_nth_from_beginning(2)
-puts "BUG: Value at index 2 should be 4 and is #{value}" if value != 4
-value = my_linked_list.find_nth_from_beginning(3)
-puts "BUG: Value at index 3 should be 5 and is #{value}" if value != 5
-value = my_linked_list.find_nth_from_beginning(1)
-puts "BUG: Value at index 1 should be 3 and is #{value}" if value != 3
+# # print all elements
+# puts "Printing elements in the linked list:"
+# my_linked_list.visit
 
-# Insert ascending
-puts "Adding 6 in ascening order."
-my_linked_list.insert_ascending(6)
+# # Insert ascending
+# puts "Adding 4 in ascending order."
+# my_linked_list.insert_ascending(4)
+# # check newly inserted value
+# puts "Checking values by calling find_nth_from_beginning method."
+# value = my_linked_list.find_nth_from_beginning(2)
+# puts "BUG: Value at index 2 should be 4 and is #{value}" if value != 4
+# value = my_linked_list.find_nth_from_beginning(3)
+# puts "BUG: Value at index 3 should be 5 and is #{value}" if value != 5
+# value = my_linked_list.find_nth_from_beginning(1)
+# puts "BUG: Value at index 1 should be 3 and is #{value}" if value != 3
 
-# print all elements
-puts "Printing elements in the linked list:"
-my_linked_list.visit
+# # Insert ascending
+# puts "Adding 6 in ascening order."
+# my_linked_list.insert_ascending(6)
 
-# vaidate length
-puts "Confirming length of the linked list."
-my_linked_list_length = my_linked_list.length
-puts "BUG: Length should be 5 and not #{my_linked_list_length}" if my_linked_list_length != 5
+# # print all elements
+# puts "Printing elements in the linked list:"
+# my_linked_list.visit
 
-# find min and max
-puts "Confirming min and max values in the linked list."
-min = my_linked_list.find_min
-puts "BUG: Min value should be 1 and not #{min}" if min != 1
-max = my_linked_list.find_max
-puts "BUG: Max value should be 5 and not #{max}" if max != 6
+# # vaidate length
+# puts "Confirming length of the linked list."
+# my_linked_list_length = my_linked_list.length
+# puts "BUG: Length should be 5 and not #{my_linked_list_length}" if my_linked_list_length != 5
 
-# delete value
-puts "Deleting node with value 5 from the linked list."
-my_linked_list.delete(5)
-# print all elements
-puts "Printing elements in the linked list:"
-my_linked_list.visit
-# validate length
-puts "Confirming length of the linked list."
-my_linked_list_length = my_linked_list.length
-puts "BUG: Length should be 4 and not #{my_linked_list_length}" if my_linked_list_length != 4
+# # find min and max
+# puts "Confirming min and max values in the linked list."
+# min = my_linked_list.find_min
+# puts "BUG: Min value should be 1 and not #{min}" if min != 1
+# max = my_linked_list.find_max
+# puts "BUG: Max value should be 5 and not #{max}" if max != 6
 
-# delete value
-puts "Deleting node with value 1 from the linked list."
-my_linked_list.delete(1)
-# print all elements
-puts "Printing elements in the linked list:"
-my_linked_list.visit
-# validate length
-puts "Confirming length of the linked list."
-my_linked_list_length = my_linked_list.length
-puts "BUG: Length should be 3 and not #{my_linked_list_length}" if my_linked_list_length != 3
+# # delete value
+# puts "Deleting node with value 5 from the linked list."
+# my_linked_list.delete(5)
+# # print all elements
+# puts "Printing elements in the linked list:"
+# my_linked_list.visit
+# # validate length
+# puts "Confirming length of the linked list."
+# my_linked_list_length = my_linked_list.length
+# puts "BUG: Length should be 4 and not #{my_linked_list_length}" if my_linked_list_length != 4
 
-# find middle element
-puts "Confirming middle value in the linked list."
-middle = my_linked_list.find_middle_value
-puts "BUG: Middle value should be 4 and not #{middle}" if middle != 4
+# # delete value
+# puts "Deleting node with value 1 from the linked list."
+# my_linked_list.delete(1)
+# # print all elements
+# puts "Printing elements in the linked list:"
+# my_linked_list.visit
+# # validate length
+# puts "Confirming length of the linked list."
+# my_linked_list_length = my_linked_list.length
+# puts "BUG: Length should be 3 and not #{my_linked_list_length}" if my_linked_list_length != 3
 
-# reverse the linked list
-puts "Reversing the linked list."
-my_linked_list.reverse
-# print all elements
-puts "Printing elements in the linked list:"
-my_linked_list.visit
-# verify the reversed list
-puts "Verifying the reveresed linked list by calling find_nth_from_beginning method."
-value = my_linked_list.find_nth_from_beginning(2)
-puts "BUG: Value at index 2 should be 3 and is #{value}" if value != 3
-value = my_linked_list.find_nth_from_beginning(1)
-puts "BUG: Value at index 1 should be 4 and is #{value}" if value != 4
-value = my_linked_list.find_nth_from_beginning(0)
-puts "BUG: Value at index 0 should be 6 and is #{value}" if value != 6
+# # find middle element
+# puts "Confirming middle value in the linked list."
+# middle = my_linked_list.find_middle_value
+# puts "BUG: Middle value should be 4 and not #{middle}" if middle != 4
 
-# nth from the end
-puts "Verifying find_nth_from_end method."
-value = my_linked_list.find_nth_from_end(0)
-puts "BUG: Value at index 0 from the end, should be 3 and is #{value}" if value != 3
-value = my_linked_list.find_nth_from_end(1)
-puts "BUG: Value at index 1 from the end, should be 4 and is #{value}" if value != 4
-value = my_linked_list.find_nth_from_end(2)
-puts "BUG: Value at index 2 from the end, should be 6 and is #{value}" if value != 6
+# # reverse the linked list
+# puts "Reversing the linked list."
+# my_linked_list.reverse
+# # print all elements
+# puts "Printing elements in the linked list:"
+# my_linked_list.visit
+# # verify the reversed list
+# puts "Verifying the reveresed linked list by calling find_nth_from_beginning method."
+# value = my_linked_list.find_nth_from_beginning(2)
+# puts "BUG: Value at index 2 should be 3 and is #{value}" if value != 3
+# value = my_linked_list.find_nth_from_beginning(1)
+# puts "BUG: Value at index 1 should be 4 and is #{value}" if value != 4
+# value = my_linked_list.find_nth_from_beginning(0)
+# puts "BUG: Value at index 0 should be 6 and is #{value}" if value != 6
 
-# check for cycle
-puts "Checking the linked list for cycle."
-puts "BUG: Should not have a cycle." if my_linked_list.has_cycle
+# # nth from the end
+# puts "Verifying find_nth_from_end method."
+# value = my_linked_list.find_nth_from_end(0)
+# puts "BUG: Value at index 0 from the end, should be 3 and is #{value}" if value != 3
+# value = my_linked_list.find_nth_from_end(1)
+# puts "BUG: Value at index 1 from the end, should be 4 and is #{value}" if value != 4
+# value = my_linked_list.find_nth_from_end(2)
+# puts "BUG: Value at index 2 from the end, should be 6 and is #{value}" if value != 6
 
-# create cycle and then test for it
-puts "Creating a cycle in the linked list."
-my_linked_list.create_cycle
-puts "Checking the linked list for cycle."
-puts "BUG: Should not have a cycle." if !(my_linked_list.has_cycle)
+# # check for cycle
+# puts "Checking the linked list for cycle."
+# puts "BUG: Should not have a cycle." if my_linked_list.has_cycle
+
+# # create cycle and then test for it
+# puts "Creating a cycle in the linked list."
+# my_linked_list.create_cycle
+# puts "Checking the linked list for cycle."
+# puts "BUG: Should not have a cycle." if !(my_linked_list.has_cycle)
